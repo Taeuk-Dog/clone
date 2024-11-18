b74a40b9d32f9b0fd5a9d8ec961f0cfff119112c
Taeuk-Dog <xodnr34@naver.com>
2024-11-18 20:13:51 +0900

    feat;
    Nico 바닐라js로 크롬앱 만들기

---
+++ app.js
@@ -0,0 +1,20 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const link = document.querySelector('a');
+
+function onLoginsubmit(event) {
+    const uservalue = loginInput.value
+    event.preventDefault();
+    console.log(uservalue);
+}
+
+function handle(event) {
+    console.dir(event)
+    event.preventDefault()
+
+}
+
+
+
+loginForm.addEventListener('submit', onLoginsubmit)
+link.addEventListener('click', handle)
